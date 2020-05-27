c95a95f3444540162fc788fae61d4057a96297a7
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-05-28 00:29:07 +0200

    Move GET Firebase logic to Containers cmp

--- vhs-kurse/src/App.js
+++ vhs-kurse/src/App.js
@@ -5,18 +5,7 @@ import styles from './App.module.css';
 import Courses from "./containers/Courses/Courses";
 
 class App extends Component {
-    state = {
-        courses: []
-    };
-
-    componentDidMount() {
-        axios.get('https://vhs-kurse.firebaseio.com/veranstaltungen/veranstaltung.json?orderBy="$key"&limitToFirst=5')
-            .then(res => this.setState({courses: res.data}));
-    }
-
     render() {
-        const courses = this.state.courses;
-
         return (
             <div className={styles.App}>
                 {/* Header */}
@@ -24,9 +13,7 @@ class App extends Component {
                 {/* SideDrawer */}
 
                 <main>
-                    <Courses
-                        courses={courses}
-                    />
+                    <Courses/>
                 </main>
             </div>
         );
--- vhs-kurse/src/containers/Courses/Courses.js
+++ vhs-kurse/src/containers/Courses/Courses.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, {Component} from "react";
 import Course from "../../components/Course/Course";
+import axios from "axios";
 
-const Courses = props => {
-    return (
-        props.courses.map(course => {
-            const desc = course.text.find(text => text.eigenschaft === 'Beschreibung').text;
-            return (
-                <Course
-                    key={course.guid}
-                    id={course.guid}
-                    title={course.name}
-                    caption={course.untertitel}
-                    price={course.preis.betrag}
-                />
-            );
-        })
-    );
+class Courses extends Component {
+    state = {
+        courses: []
+    };
+
+    componentDidMount() {
+        axios.get('https://vhs-kurse.firebaseio.com/veranstaltungen/veranstaltung.json?orderBy="$key"&limitToFirst=5')
+            .then(res => this.setState({courses: res.data}));
+    }
+
+    render() {
+        return (
+            this.state.courses.map(course => {
+                const desc = course.text.find(text => text.eigenschaft === 'Beschreibung').text;
+                return (
+                    <Course
+                        key={course.guid}
+                        id={course.guid}
+                        title={course.name}
+                        caption={course.untertitel}
+                        price={course.preis.betrag}
+                    />
+                );
+            })
+        );
+    }
 };
 
 export default Courses;
