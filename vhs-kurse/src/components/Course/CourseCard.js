881700c42a8c352c9153a62b56e9cd5f6ab9e404
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-06-01 09:22:26 +0200

    Add Css Grid for CourseCard cmp

--- vhs-kurse/src/components/Course/CourseCard.js
+++ vhs-kurse/src/components/Course/CourseCard.js
@@ -4,15 +4,17 @@ import styles from './CourseCard.module.css';
 const CourseCard = (props) => {
     return (
         <div className={styles.card}>
+            <div className={styles.cardTitle}>
+                <h3>{props.title}s</h3>
+            </div>
             <div className={styles.courseImage}></div>
             <div className={styles.coursePreview}>
-                <h3>{props.title}s</h3>
                 <p>{props.caption}</p>
                 <strong>{props.price} €</strong>
-                <div className={styles.cardCtrl}>
-                    <p>Mehr</p>
-                    <p>Hinzufügen</p>
-                </div>
+            </div>
+            <div className={styles.cardCtrl}>
+                <p>Mehr</p>
+                <p>Hinzufügen</p>
             </div>
         </div>
     );
