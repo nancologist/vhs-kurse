c69c29e02d52485c40275d5f1d20593df258143b
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-06-01 08:47:52 +0200

    Amend Styling

--- vhs-kurse/src/components/Search/Search.js
+++ vhs-kurse/src/components/Search/Search.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 class Search extends Component {
     render() {
         return (
-            <h1>SideBar - Advance Search</h1>
+            <h1>SideBar</h1>
         );
     }
 }
